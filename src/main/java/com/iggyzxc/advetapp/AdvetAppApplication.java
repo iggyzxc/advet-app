d3be3101db4837fbe2935a3004ac2e8ae880336c
Ignacio Macam Jr. <ignaciomacam@gmail.com>
2024-09-26 21:11:04 +0800

    first commit

---
+++ src/main/java/com/iggyzxc/advetapp/AdvetAppApplication.java
@@ -0,0 +1,13 @@
+package com.iggyzxc.advetapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvetAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvetAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iggyzxc/advetapp/AdvetAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iggyzxc.advetapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvetAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
